33833f17bdccb374583784fd911bc82478d02380
sonam079 <s.shreyna079@gmail.com>
2018-06-05 14:10:17 +0530

    First Commit

---
+++ app/src/main/java/com/example/sonam/mapsdemo/CostomerHomePage.java
@@ -0,0 +1,13 @@
+package com.example.sonam.mapsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CostomerHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_costomer_home_page);
+    }
+}
---
+++ app/src/main/java/com/example/sonam/mapsdemo/CustomersLocation.java
@@ -0,0 +1,13 @@
+package com.example.sonam.mapsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomersLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customers_location);
+    }
+}
